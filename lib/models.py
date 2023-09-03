6bfa4801b2489d11624fc63daa80dbae93f5f9c1
JOY19ANYANGO <joyanyango402@gmail.com>
2023-09-03 23:29:56 +0300

    made few changes

--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData,Float,create_engine
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Float, create_engine
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
@@ -11,11 +12,12 @@ Base = declarative_base(metadata=metadata)
 engine = create_engine('sqlite:///restuarants.db')
 Session = sessionmaker(bind=engine)
 session = Session()
+
 class Restaurant(Base):
-    __tablename__="restaurants"
-    id=Column(Integer,primary_key=True)
-    name=Column(String)
-    price=Column(Float,nullable=False)
+    __tablename__ = "restaurants"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Float, nullable=False)
 
     def __repr__(self):
         return f'<Restaurant {self.name}>'
@@ -26,14 +28,19 @@ class Restaurant(Base):
         reviews=session.query(Review).filter_by(restaurant_id=self.id).all()
         return [review.customer for review in reviews]
 
+
+    # Define the relationship with Review explicitly
+    reviews = relationship("Review", back_populates="restaurant")
+
 class Customer(Base):
-    __tablename__="customers"  
-    id=Column(Integer,primary_key=True) 
-    first_name=Column(String) 
-    last_name=Column(String)
-    
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
     def __repr__(self):
         return f"<Customer {self.first_name} ,{self.last_name}>"
+    
     def customer_reviews(self):
         reviews=session.query(Review).filter_by(customer_id=self.id).all()
         return reviews
@@ -42,23 +49,27 @@ class Customer(Base):
         reviews=session.query(Review).filter_by(customer_id=self.id).all()
         return [review.restaurant for review in reviews]
 
+    # Define the relationship with Review explicitly
+    reviews = relationship("Review", back_populates="customer")
+    restaurants=relationship("Review",back_populates="customer",overlaps="reviews")
+
 class Review(Base):
     __tablename__ = "reviews"
-    
+
     id = Column(Integer, primary_key=True)
     star_rating = Column(Integer)
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
     customer_id = Column(Integer, ForeignKey('customers.id'))
-    
-    restaurant = relationship("Restaurant", backref=backref("reviews"))
-    customer = relationship("Customer", backref=backref("reviews"))
-    
+
+    # Define the relationships explicitly
+    restaurant = relationship("Restaurant", back_populates="reviews")
+    customer = relationship("Customer", back_populates="reviews")
+
     def review_customer(self):
-        
         return self.customer
+
     def review_restaurant(self):
-        
         return self.restaurant
-    
+
     def __repr__(self):
-        return f"<Review ,Star rating:{self.star_rating}>"
+        return f"<Review, Star rating: {self.star_rating}>"
