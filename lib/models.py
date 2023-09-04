78736be729c5cfaa7ddc0063bd5c4ab26f317e7d
JOY19ANYANGO <joyanyango402@gmail.com>
2023-09-04 21:30:48 +0300

    added comments

--- lib/models.py
+++ lib/models.py
@@ -13,31 +13,44 @@ engine = create_engine('sqlite:///restuarants.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# restaurant model
 class Restaurant(Base):
+    # create restaurant table
     __tablename__ = "restaurants"
+    # add restaurant column
     id = Column(Integer, primary_key=True)
     name = Column(String)
     price = Column(Float, nullable=False)
     
+     # Define the relationship with Review explicitly
     reviews = relationship("Review", back_populates="restaurant")
+    
+    # make restaurant object readable
     def __repr__(self):
         return f'<Restaurant name:{self.name}, price:{self.price}>'
+    
+    # return all reviews for a specific restaurant 
     def restaurant_reviews(self):
         reviews=session.query(Review).filter_by(restaurant_id=self.id).all()
         return reviews
+    
+    # return customers who have made reviews for a specific restaurant
     def restaurant_customers(self):
         reviews=session.query(Review).filter_by(restaurant_id=self.id).all()
         return [review.customer for review in reviews]
+    
+    # return most expensive restaurant
     @classmethod
     def fanciest_restaurants(cls):
         restaurant=session.query(Restaurant).order_by(Restaurant.price.desc()).first()
         return restaurant
     
+    # return all reviews for a given restaurant in a specific format
     def all_reviews(self):
         reviews = session.query(Review).filter_by(restaurant_id=self.id).all()
         return [review.full_review() for review in reviews]
        
-    # Define the relationship with Review explicitly
+   
    
 
 class Customer(Base):
@@ -48,23 +61,29 @@ class Customer(Base):
     
     reviews = relationship("Review", back_populates="customer")
     restaurants=relationship("Review",back_populates="customer",overlaps="reviews")
-
+    
+ # make customer object readable
     def __repr__(self):
         return f"<Customer {self.first_name} ,{self.last_name}>"
     
+    # return all reviews for a specific customer
     def customer_reviews(self):
         reviews=session.query(Review).filter_by(customer_id=self.id).all()
         return reviews
-    
+    # return all restaurant reviewed for a specific customer
     def customer_restaurants(self):
         reviews=session.query(Review).filter_by(customer_id=self.id).all()
         return [review.restaurant for review in reviews]
+    
+    # returns customers full name
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
+    # return reviewed restaurant with highest rating for a specific customer
     def favorite_restaurant(self):
         review=session.query(Review).filter_by(customer_id=self.id).order_by(Review.star_rating.desc()).limit(1).first()
         return review.restaurant
-    
+    # adds review and makes it persist in the database
     def add_review(self, restaurant, rating):
       new_review = Review(
         star_rating=rating,
@@ -75,6 +94,7 @@ class Customer(Base):
       session.commit()
       return "Review added successfully"
   
+  # delete review from the database
     def delete_review(self, restaurant):
       print(f"Deleting reviews for customer_id={self.id} and restaurant_id={restaurant}")
       reviews = session.query(Review).filter_by(customer_id=self.id, restaurant_id=restaurant).all()
@@ -101,15 +121,20 @@ class Review(Base):
     # Define the relationships explicitly
     restaurant = relationship("Restaurant", back_populates="reviews")
     customer = relationship("Customer", back_populates="reviews")
-
+    
+    # return customer for a specific review
     def review_customer(self):
         return self.customer
 
+    # return restaurant for a specific review
     def review_restaurant(self):
         return self.restaurant
-
+    
+    # make review object readable
     def __repr__(self):
         return f"<Review, Star rating: {self.star_rating},customer id:{self.customer_id},restaurant_id:{self.restaurant_id}>"
+    
+    # return review in a specific format
     def full_review(self):
         return f"Review for {self.customer.full_name()} by {self.restaurant.name} : {self.star_rating} stars"
 
